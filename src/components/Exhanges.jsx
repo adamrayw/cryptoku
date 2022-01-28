c55563e64cf8daaa3594bd23993609fb80c0f87e
Adam Ray Wibowo <raywibowo68@gmail.com>
2022-01-28 11:26:10 +0700

    Redesign using Swiper

--- src/components/Exhanges.jsx
+++ src/components/Exhanges.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
+// import Swiper core and required modules
+import SwiperCore, {
+    Autoplay, Pagination, Navigation
+} from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([Autoplay, Pagination, Navigation]);
 
 export default function Exhanges() {
     const [exchanges, setExchanges] = useState([])
@@ -55,18 +65,49 @@ export default function Exhanges() {
                         <div className='w-full h-4 rounded-full animate-pulse bg-gray-200'></div>
                     </div>
                 </div>
-            </>) : (<div className='grid md:grid-cols-4 grid-cols-2 md:gap-x-6 gap-4 items-center justify-around'>
-                {exchanges.map((ex) => {
-                    return (
-                        <a href={ex.url} class="block py-6 px-4 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
-                            <div className='flex items-center'>
-                                <img src={ex.image} alt="ex" class="md:h-12 md:w-12 w-6 h-6 rounded-full" />
-                                <h5 class="md:text-xl ml-4 text-sm font-bold tracking-tight text-gray-800">{ex.name}</h5>
-                            </div>
-                        </a>
-                    )
-                })}
+            </>) : (<div className=''>
+                <Swiper
+
+                    autoplay={{
+                        "delay": 1600,
+                        "disableOnInteraction": false
+                    }}
+                    spaceBetween={50}
+                    breakpoints={{
+                        "240": {
+                            "slidesPerView": 2,
+                            "spaceBetween": 6
+                        },
+                        "640": {
+                            "slidesPerView": 2,
+                            "spaceBetween": 20
+                        },
+                        "768": {
+                            "slidesPerView": 2,
+                            "spaceBetween": 40
+                        },
+                        "1024": {
+                            "slidesPerView": 4,
+                            "spaceBetween": 50
+                        }
+                    }}
+
+                >
+                    {exchanges.map((ex) => {
+                        return (
+                            <SwiperSlide>
+                                <a href={ex.url} class="block my-2 mx-2 py-6 px-4 h-fit -max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
+                                    <div className='flex items-center'>
+                                        <img src={ex.image} alt="ex" class="md:h-12 md:w-12 w-6 h-6 rounded-full" />
+                                        <h5 class="md:text-xl text-center ml-4 text-sm font-bold tracking-tight text-gray-800 truncate overflow-hidden">{ex.name}</h5>
+                                    </div>
+                                </a>
+                            </SwiperSlide>
+                        )
+                    })}
+                </Swiper>
             </div>)}
+
         </div>
 
     );
